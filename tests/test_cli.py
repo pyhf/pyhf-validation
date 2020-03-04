061a0af033af1ba12e86f1f6347e148fff14f8d0
Matthew Feickert <matthew.feickert@cern.ch>
2020-03-04 10:45:14 -0600

    Correct CLI tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,7 +4,7 @@ import time
 
 
 def test_version(script_runner):
-    command = "hfval --version"
+    command = "pyhf-validation --version"
     start = time.time()
     ret = script_runner.run(*shlex.split(command))
     end = time.time()
