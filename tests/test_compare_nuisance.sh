5506c1997f05c1f4a466db93db37d24721f24d5d
Matthew Feickert <matthew.feickert@cern.ch>
2020-03-07 14:12:08 -0600

    Apply shellcheck recommendations
    c.f.:
    - https://github.com/koalaman/shellcheck/wiki/SC2128
    - https://github.com/koalaman/shellcheck/wiki/SC2086

--- tests/test_compare_nuisance.sh
+++ tests/test_compare_nuisance.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get the location of this testing script to make other paths relative to it
-scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+scriptDir=$(dirname -- "$(readlink -f -- "${BASH_SOURCE[@]}")")
 
 # Download test files
 curl https://cernbox.cern.ch/index.php/s/eDsgyIlqhu1OVTP/download --output sample_root_workspace.root
@@ -12,7 +12,7 @@ curl https://cernbox.cern.ch/index.php/s/YoCnn78sIwWCSoV/download --output compa
 
 # Test the python script compare_nuisance.py
 echo "################## Testing python script compare_nuisance.py with sample input files ##################"
-python ${scriptDir}/../scripts/compare_nuisance.py --root-workspace sample_root_workspace.root --pyhf-json sample_pyhf_likelihood.json | tee compare_nuisance_output.txt
+python "${scriptDir}"/../scripts/compare_nuisance.py --root-workspace sample_root_workspace.root --pyhf-json sample_pyhf_likelihood.json | tee compare_nuisance_output.txt
 
 # Compare the output files with the expected ones
 if cmp -s "compare_nuisance_output_expected.txt" "compare_nuisance_output.txt" ; then
@@ -25,7 +25,7 @@ echo "##########################################################################
 
 # Test the python script compare_fitted_nuisance.py
 echo "############### Testing python script compare_fitted_nuisance.py with sample input files ##############"
-python ${scriptDir}/../scripts/compare_fitted_nuisance.py --root-workspace sample_root_workspace.root --pyhf-json sample_pyhf_likelihood.json
+python "${scriptDir}"/../scripts/compare_fitted_nuisance.py --root-workspace sample_root_workspace.root --pyhf-json sample_pyhf_likelihood.json
 
 
 # Remove the test files and output files now that we're done with them
