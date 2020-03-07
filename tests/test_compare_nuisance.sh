3a98207e36c6ca670a7d1f1b8df8e68feb4d3ef0
Matthew Feickert <matthew.feickert@cern.ch>
2020-03-07 14:21:04 -0600

    Seperate options with newlines for readability

--- tests/test_compare_nuisance.sh
+++ tests/test_compare_nuisance.sh
@@ -12,7 +12,10 @@ curl https://cernbox.cern.ch/index.php/s/YoCnn78sIwWCSoV/download --output compa
 
 # Test the python script compare_nuisance.py
 echo "################## Testing python script compare_nuisance.py with sample input files ##################"
-python "${scriptDir}"/../scripts/compare_nuisance.py --root-workspace sample_root_workspace.root --pyhf-json sample_pyhf_likelihood.json | tee compare_nuisance_output.txt
+python "${scriptDir}"/../scripts/compare_nuisance.py \
+  --root-workspace sample_root_workspace.root \
+  --pyhf-json sample_pyhf_likelihood.json \
+  | tee compare_nuisance_output.txt
 
 # Compare the output files with the expected ones
 if cmp -s "compare_nuisance_output_expected.txt" "compare_nuisance_output.txt" ; then
@@ -25,7 +28,9 @@ echo "##########################################################################
 
 # Test the python script compare_fitted_nuisance.py
 echo "############### Testing python script compare_fitted_nuisance.py with sample input files ##############"
-python "${scriptDir}"/../scripts/compare_fitted_nuisance.py --root-workspace sample_root_workspace.root --pyhf-json sample_pyhf_likelihood.json
+python "${scriptDir}"/../scripts/compare_fitted_nuisance.py \
+  --root-workspace sample_root_workspace.root \
+  --pyhf-json sample_pyhf_likelihood.json
 
 
 # Remove the test files and output files now that we're done with them
